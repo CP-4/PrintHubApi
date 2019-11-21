56b03eafbf4c50bf43eb2669a54f3b38be748bfa
CP-4 <chinmaypatel02@gmail.com>
2019-11-21 16:10:33 +0530

    Added: Fields to Document and CustomUser Models
    Document:
    - docname
    - student_name
    - print_copies
    - print_feature
    
    CustomUser:
    - student_name

--- file2/models.py
+++ file2/models.py
@@ -12,15 +12,33 @@ def get_sentinel_user():
 
 
 class CustomUser(AbstractUser):
+    student_name = models.CharField(max_length=100, default='student_name')
     pass
 
 
 class Document(models.Model):
 
+    SINGLESIDE = "SINGLESIDE"
+    DOUBLESIDE = "DOUBLESIDE"
+    BOOKSINGLESIDE = "BOOKSINGLESIDE"
+    BOOKDOUBLESIDE = "BOOKDOUBLESIDE"
+
+    PRINT_FEATURE_CHOICES = (
+        (SINGLESIDE, "Single Side"),
+        (DOUBLESIDE, "Double Side"),
+        (BOOKSINGLESIDE, "Book Single Side"),
+        (BOOKDOUBLESIDE, "Book Double Side"),
+    )
+
     docfile = models.FileField(upload_to='documents/%Y/%m/%d')
     printJobStatus = models.IntegerField(default=0)
     pages = models.IntegerField(default=0)
     doctype = models.CharField(max_length=5, default='err')
+    docname = models.CharField(max_length=20, default='docname')
+    student_name = models.CharField(max_length=100, default='student_name')
+
+    print_copies = models.IntegerField(default=1)
+    print_feature = models.CharField(max_length=20, choices=PRINT_FEATURE_CHOICES, default=SINGLESIDE)
 
     student = models.ForeignKey(
         settings.AUTH_USER_MODEL,
@@ -29,11 +47,15 @@ class Document(models.Model):
 
     def get_document_type(self):
         filename, file_extension = os.path.splitext(self.docfile.name)
-        print(self.docfile.name)
-        print(filename, file_extension)
         return file_extension
+
+    def get_document_name(self):
+        filename, file_extension = os.path.splitext(self.docfile.name)
+        return filename
 
     def save(self, *args, **kwargs):
 
         self.doctype = self.get_document_type()
+        self.filename = self.get_document_name()
+        self.student_name = self.student.student_name;
         super().save(*args, **kwargs)  # Call the "real" save() method.
